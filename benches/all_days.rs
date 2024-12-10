68fcf02fccb00f46fed2071af0890104bc1aa284
Hubert Banaszewski <hubert.banaszewski@arista.com>
2024-12-10 18:37:13 +0100

    Add all_days benchmark

--- benches/all_days.rs
+++ benches/all_days.rs
@@ -1,12 +1,20 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    for mut day in aoc2024::all_days() {
+    let mut all_days = aoc2024::all_days();
+    for day in all_days.iter_mut() {
         day.load_input();
         c.bench_function(format!("{:02}", day.number()).as_str(), |b| {
             b.iter(|| day.solve())
         });
     }
+    c.bench_function("all_days", |b| {
+        b.iter(|| {
+            all_days.iter().for_each(|d| {
+                d.solve();
+            })
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
